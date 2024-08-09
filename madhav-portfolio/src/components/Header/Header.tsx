4f6fe8cfd8c9875fc5493390dc77e9890e8334f3
madhavtrivedi77 <madhavtrivedi.77@gmail.com>
2024-08-10 02:11:42 +0530

    Initial commit with home section

---
+++ madhav-portfolio/src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import styles from "./header.module.scss";
+// import 'bootstrap/dist/css/bootstrap.css';
+
+export default function Header() {
+    return <nav className="navbar sticky-top navbar-expand-lg navbar-light position-absolute w-100">
+    <div className={`container-fluid ${styles.navbar_content_center}`}>
+      {/* <a className="navbar-brand playfair-font" href="#">Navbar scroll</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button> */}
+      <div className="collapse navbar-collapse" id="navbarScroll">
+        <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+          <li className="nav-item">
+            <a className="nav-link active playfair-font" aria-current="page" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link playfair-font" href="#">Journey</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link playfair-font" href="#">Testimonials</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link playfair-font" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+}
